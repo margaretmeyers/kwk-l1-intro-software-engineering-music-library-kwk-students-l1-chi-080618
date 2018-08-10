808756225c560f12955954d293f41486bdf6db01
margaretmeyers <marg.m.meyers@gmail.com>
2018-08-10 13:56:56 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -14,7 +14,8 @@ def list_artists
 end 
 
     
-  return list_artists(@library).keys
+  return @library.keys
+  
 
 
 #2. Define a method named `list_songs`. This method should use the `each_value` method to iterate over the music library hash and print out the name of the songs in the library.
